f8c5b2f5894ffc8ab4c5392cd4a55678131788fe
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-04 00:42:03 -0400

    chore: initial setup

---
+++ src/cypress-dark-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const cypressDark = require('.')
+
+describe('cypress-dark', () => {
+  it('write this test', () => {
+    console.assert(cypressDark, 'should export something')
+  })
+})

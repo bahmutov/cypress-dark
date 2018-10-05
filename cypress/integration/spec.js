09b526f500b3cafa37661eb94efd4155708ba2bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-05 01:04:46 -0400

    Halloween theme (#9)
    * feat: CSS variables via post- processing, close #7
    
    * enable tests
    
    * chore: do not print transformed CSS
    
    * feat: add Halloween theme, close #6
    
    * fix: remove duplicate function definition

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,25 +1,33 @@
 /// <reference types="cypress" />
 describe('devdocs.io', () => {
+  // beforeEach(() => {
+  //   // use devdocs.io in dark mode for demo
+  //   cy.visit('https://devdocs.io/')
+  //   cy.setCookie('dark', '1').setCookie('hideIntro', '1')
+  //   // let the page load
+  //   cy.contains('._list-text', 'JavaScript').should('be.visible')
+  // })
   beforeEach(() => {
-    // use devdocs.io in dark mode for demo
-    cy.visit('https://devdocs.io/')
-    cy.setCookie('dark', '1').setCookie('hideIntro', '1')
-    // let the page load
-    cy.contains('._list-text', 'JavaScript').should('be.visible')
+    cy.visit('http://www.halloween-website.com/').wait(300)
   })
 
-  it('loads JavaScript docs', () => {
-    cy.get('input[name=q]').type('JavaScript{enter}', {
-      delay: 50
-    })
-    cy.url().should('contain', '/javascript/')
-    cy.get('._content-loading').should('not.be.visible')
-    cy.contains('._list-item', 'JavaScript').should('have.class', 'active')
-  })
+  // it.skip('loads JavaScript docs', () => {
+  //   cy.get('input[name=q]').type('JavaScript{enter}', {
+  //     delay: 50
+  //   })
+  //   cy.url().should('contain', '/javascript/')
+  //   cy.get('._content-loading').should('not.be.visible')
+  //   cy.contains('._list-item', 'JavaScript').should('have.class', 'active')
+  // })
 
   it('has failing test if needed', () => {
+    // cy.wait(10000000)
     expect(1).to.equal(1)
     // expect(1).to.equal(2)
+  })
+
+  it('has passing test', () => {
+    expect(1).to.equal(1)
   })
 
   it('has skipped test')

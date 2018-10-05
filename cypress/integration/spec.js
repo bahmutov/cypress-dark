5b830b4fb28c713907311eb3411d29f0391c068b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-05 00:04:08 -0400

    enable tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,14 +7,20 @@ describe('devdocs.io', () => {
     // let the page load
     cy.contains('._list-text', 'JavaScript').should('be.visible')
   })
+
   it('loads JavaScript docs', () => {
-    // cy.get('input[name=q]').type('JavaScript{enter}', {
-    //   delay: 50
-    // })
-    // cy.url().should('contain', '/javascript/')
-    // cy.get('._content-loading').should('not.be.visible')
-    // cy.contains('._list-item', 'JavaScript').should('have.class', 'active')
-    expect(1).to.equal(1)
-    expect(1).to.equal(2)
+    cy.get('input[name=q]').type('JavaScript{enter}', {
+      delay: 50
+    })
+    cy.url().should('contain', '/javascript/')
+    cy.get('._content-loading').should('not.be.visible')
+    cy.contains('._list-item', 'JavaScript').should('have.class', 'active')
   })
+
+  it('has failing test if needed', () => {
+    expect(1).to.equal(1)
+    // expect(1).to.equal(2)
+  })
+
+  it('has skipped test')
 })

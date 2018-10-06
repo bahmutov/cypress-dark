c1f0e92b895f9a96ea4bdd769bd17623c642e318
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-06 12:10:27 -0400

    feat: halloween theme laughs if run fails, close #13 (#14)

--- src/index.js
+++ src/index.js
@@ -13,6 +13,18 @@ const knownThemes = ['dark', 'halloween']
 
 const isStyleLoaded = $head => $head.find('#cypress-dark').length > 0
 
+const getTheme = () => Cypress._.toLower(Cypress.config('theme') || 'dark')
+
+const getSourceFolder = () => {
+  // if source folder starts with /../...
+  // the the user package has installed it using relative "file:.." link
+  const installedAsFile = Cypress._.startsWith(__dirname, '/..')
+  const sourceFolder = installedAsFile
+    ? join('node_modules/cypress-dark/src')
+    : __dirname
+  return sourceFolder
+}
+
 /* eslint-env mocha, browser */
 /* global Cypress, cy */
 before(() => {
@@ -22,7 +34,7 @@ before(() => {
     return
   }
 
-  let theme = Cypress._.toLower(Cypress.config('theme') || 'dark')
+  let theme = getTheme()
 
   if (!knownThemes.includes(theme)) {
     console.error(
@@ -34,7 +46,7 @@ before(() => {
     console.error('using default theme "%s"', theme)
   }
 
-  const themeFilename = join(__dirname, `${theme}.css`)
+  const themeFilename = join(getSourceFolder(), `${theme}.css`)
 
   cy
     .readFile(themeFilename, { log: false })
@@ -45,4 +57,37 @@ before(() => {
         `<style type="text/css" id="cypress-dark" theme="${theme}">\n${css}</style>`
       )
     })
+})
+
+const hasFailed = () => {
+  const rootSuite = getRootTest(Cypress.state('ctx').currentTest)
+  const failed = hasSuiteFailed(rootSuite)
+  return failed
+}
+
+const isHalloween = () => getTheme() === 'halloween'
+
+const witchLaughs = () => {
+  const filename = join(getSourceFolder(), 'halloween-laugh.mp3')
+  cy.readFile(filename, 'base64', { log: false }).then(mp3 => {
+    const uri = 'data:audio/mp3;base64,' + mp3
+    const audio = new Audio(uri)
+    audio.play()
+  })
+}
+
+const getRootTest = test => {
+  return test.parent ? getRootTest(test.parent) : test
+}
+
+const hasTestFailed = test => test.state === 'failed'
+
+const hasSuiteFailed = suite => {
+  return suite.tests.some(hasTestFailed) || suite.suites.some(hasSuiteFailed)
+}
+
+after(() => {
+  if (hasFailed() && isHalloween()) {
+    witchLaughs()
+  }
 })

de973d056caf6b9cb427afee32605b4ede254c4f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-29 09:24:15 -0400

    fix: update the Halloween theme

--- cypress/integration/halloween-spec.js
+++ cypress/integration/halloween-spec.js
@@ -9,29 +9,21 @@ require('../../src/halloween')
   Several tests that show how Cypress test runner looks with
   a custom color theme
 */
-describe('Cypress themes', () => {
-  // beforeEach(() => {
-  //   // use devdocs.io in dark mode for demo
-  //   cy.visit('https://devdocs.io/')
-  //   cy.setCookie('dark', '1').setCookie('hideIntro', '1')
-  //   // let the page load
-  //   cy.contains('._list-text', 'JavaScript').should('be.visible')
-  // })
+describe('Cypress themes', { baseUrl: 'http://www.halloween-website.com/' }, () => {
   beforeEach(() => {
-    cy.visit('http://www.halloween-website.com/').wait(300)
+    // do not let the site load any ads
+    cy.intercept('GET', 'https://pagead2.googlesyndication.com', {
+      body: ''
+    }).as('ads')
   })
 
-  // it.skip('loads JavaScript docs', () => {
-  //   cy.get('input[name=q]').type('JavaScript{enter}', {
-  //     delay: 50
-  //   })
-  //   cy.url().should('contain', '/javascript/')
-  //   cy.get('._content-loading').should('not.be.visible')
-  //   cy.contains('._list-item', 'JavaScript').should('have.class', 'active')
-  // })
+  before(() => {
+    cy.visit('/')
+    cy.get('#camera_wrap').should('be.visible')
+      .wait(2000)
+  })
 
   it('has failing test if needed', () => {
-    // cy.wait(10000000)
     expect(1).to.equal(1)
     // expect(1).to.equal(2)
   })
